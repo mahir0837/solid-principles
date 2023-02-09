6b3712568dfa4b19b88f0bee29c0611176b4424f
owner <mahirsarac89@gmail.com>
2023-02-08 21:35:50 -0500

    good Example UserController

---
+++ src/com/sarac/singleResponsibility/good/UserController.java
@@ -0,0 +1,19 @@
+package com.sarac.singleResponsibility.good;
+
+public class UserController {
+
+    public static void getRequest(){
+
+        // Get request
+        // Send this app request to validation and security Part
+    }
+
+    public static String createResponse(){
+
+        return "Response"; //For example "User" is created
+    }
+    public static void sendResponse(){
+
+        // Send user a response
+    }
+}
