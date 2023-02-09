2a28bf0a7b000b6939dd5bfe88839d7535cb4f34
owner <mahirsarac89@gmail.com>
2023-02-08 21:40:10 -0500

    Manager

---
+++ src/com/sarac/singleResponsibility/good/Manager.java
@@ -0,0 +1,17 @@
+package com.sarac.singleResponsibility.good;
+
+public class Manager {
+
+    public static boolean validate(){
+
+        // Check validation
+        return true;
+    }
+
+    public static boolean checkSecurity(){
+
+        // Send to security related part
+
+        return true;
+    }
+}
