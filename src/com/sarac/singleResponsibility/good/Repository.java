264409f2183e8ff320e754e0e70a606edbcd07a2
owner <mahirsarac89@gmail.com>
2023-02-08 21:47:12 -0500

    Repository class created

---
+++ src/com/sarac/singleResponsibility/good/Repository.java
@@ -0,0 +1,31 @@
+package com.sarac.singleResponsibility.good;
+
+public class Repository {
+
+    public static void create(){
+
+        // Connect to database
+
+        // Create user in database
+    }
+
+    public static void read(){
+        // Read to database
+
+        // Create user in database
+
+
+    } public static void update(){
+        // Update to database
+
+        // Create user in database
+
+
+    } public static void delete(){
+
+        // Delete to database
+
+        // Create user in database
+
+    }
+}
